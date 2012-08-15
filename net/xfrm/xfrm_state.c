c649f9ad93cac4ab49b35fe0b5f3f3d6919f5e3e
Fan Du <fan.du@windriver.com>
2012-08-15 10:13:47 +0800

    xfrm: remove redundant parameter "int dir" in struct xfrm_mgr.acquire
    Sematically speaking, xfrm_mgr.acquire is called when kernel intends to ask
    user space IKE daemon to negotiate SAs with peers. IOW the direction will
    *always* be XFRM_POLICY_OUT, so remove int dir for clarity.
    
    Change-Id: Id4df779e818c5b6f5eccb9bf8250c17eb729f101
    Signed-off-by: Fan Du <fan.du@windriver.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/xfrm/xfrm_state.c
+++ net/xfrm/xfrm_state.c
@@ -1687,8 +1687,8 @@ int km_query(struct xfrm_state *x, struct xfrm_tmpl *t, struct xfrm_policy *pol)
 	int err = -EINVAL, acqret;
 	struct xfrm_mgr *km;
 
-	rcu_read_lock();
-	list_for_each_entry_rcu(km, &xfrm_km_list, list) {
+	read_lock(&xfrm_km_lock);
+	list_for_each_entry(km, &xfrm_km_list, list) {
 		acqret = km->acquire(x, t, pol);
 		if (!acqret)
 			err = acqret;
