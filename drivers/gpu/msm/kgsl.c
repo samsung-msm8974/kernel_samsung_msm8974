834833f2a5b2cf1a46b7f9ccd945eb4f0a805711
Jordan Crouse <jcrouse@codeaurora.org>
2013-11-25 16:24:40 -0700

    msm: kgsl: Mark local functions as static and remove unused code
    Mark a bunch of functions and structures as static and remove unused
    functions.
    
    Change-Id: Ic0dedbadd5520f95293905cf23afec835c786ed7
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Heavily adapted for K3.4 MSM GPU (keeping more code)
    Signed-off-by: D. Andrei Măceș <dmaces@nd.edu>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -1036,7 +1036,7 @@ error:
 	return NULL;
 }
 
-int kgsl_close_device(struct kgsl_device *device)
+static int kgsl_close_device(struct kgsl_device *device)
 {
 	int result = 0;
 	device->open_count--;
@@ -1056,7 +1056,6 @@ int kgsl_close_device(struct kgsl_device *device)
 	return result;
 
 }
-EXPORT_SYMBOL(kgsl_close_device);
 
 static int kgsl_release(struct inode *inodep, struct file *filep)
 {
@@ -1125,7 +1124,7 @@ static int kgsl_release(struct inode *inodep, struct file *filep)
 	return result;
 }
 
-int kgsl_open_device(struct kgsl_device *device)
+static int kgsl_open_device(struct kgsl_device *device)
 {
 	int result = 0;
 	if (device->open_count == 0) {
@@ -1160,7 +1159,6 @@ err:
 
 	return result;
 }
-EXPORT_SYMBOL(kgsl_open_device);
 
 static int kgsl_open(struct inode *inodep, struct file *filep)
 {
@@ -3433,7 +3431,7 @@ kgsl_ioctl_sharedmem_flush_cache(struct kgsl_device_private *dev_priv,
 /*
  * The common parts of kgsl_ioctl_gpumem_alloc and kgsl_ioctl_gpumem_alloc_id.
  */
-int
+static int
 _gpumem_alloc(struct kgsl_device_private *dev_priv,
 		struct kgsl_mem_entry **ret_entry,
 		unsigned int size, unsigned int flags)
